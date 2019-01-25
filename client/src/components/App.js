371e08adba73ed2e11be5664bcfccb0b237fbcbb
Alvin.Abhinav <alvin.abhinav@ithands.net>
2019-01-25 13:03:09 +0530

    add react filter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React,{Component} from 'react';
-import {Box,Heading,Container,Image,Card,Text} from "gestalt";
+import {Box,Heading,Container,Image,Card,Text,SearchField,Icon,Spinner} from "gestalt";
 import { Link } from "react-router-dom";
 import Strapi from 'strapi-sdk-javascript/build/main';
 const apiUrl=process.env.API_URL || 'http://localhost:1337';
 const strapi= new Strapi(apiUrl);
 class App extends Component {
  state ={
-   brands:[]
+  brands: [],
+  searchTerm: "",
+  loadingBrands:true
  }
  
   async componentDidMount(){
@@ -33,16 +35,45 @@ query:`query {
 
     });
     console.log(response); 
-    this.setState({brands:response.data.brands})
+    this.setState({brands:response.data.brands,loadingBrands:false})
   }catch(err){
     console.log(err);
+    this.setState({loadingBrands:false});
   }
  
   }
+  handleChange=({value})=>{
+    this.setState({searchTerm:value});
+  };
+  filteredBrands=({searchTerm,brands})=>{
+    return brands.filter(brand => {
+  
+      return brand.name.toString().toLowerCase().includes(searchTerm)||brand.description.toString().toLowerCase().includes(searchTerm);
+    });
+  
+  };
   render() {
-    const {brands}=this.state;
+    const {searchTerm ,loadingBrands}=this.state;
     return (
     <Container>
+<Box display="flex" justifyContent="center" margin={4} > 
+  <SearchField
+  id="seachField"
+  
+  accessibilityLabel="Brands search field"
+  onChange={this.handleChange}
+  value={searchTerm}
+  placeholder="Search Brands"
+  />
+  <Box margin={2} >
+<Icon icon="filter"
+color={searchTerm?'orange':'gray'} 
+size={20}
+accessibilityLabel="search"
+/>
+  </Box>
+</Box>
+
       <Box
       display="flex"
       justifyContent="center"
@@ -54,13 +85,21 @@ Mac Brands
       </Box>
   {/*Brands */}  
   
-  <Box display="flex" justifyContent="around">
-          {brands.map(brand => (
-            <Box margin={2} width={200} key={brand.id}>
+  <Box wrap
+    dangerouslySetInlineStyle={{
+      __style: {
+        backgroundColor: "#d6c8ec"
+      }
+    }}
+    shape="rounded"
+  display="flex" justifyContent="around">
+          {this.filteredBrands(this.state).map(brand => (
+            <Box paddingY={4} margin={2} width={200} key={brand.id}>
               <Card
                 image={
                   <Box height={200} width={200}>
                     <Image
+                        fit="cover"
                       alt="Brand"
                       naturalHeight={1}
                       naturalWidth={1}
@@ -88,6 +127,7 @@ Mac Brands
           ))}
   </Box>
   {/* End Brands */}
+  <Spinner show={loadingBrands} accessibilityLabel="loading Spinner" />
   </Container>
     );
   }
